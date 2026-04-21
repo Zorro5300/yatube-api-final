62396bbf8d4a983918aa8474ad5bf1380822dad1
Ilja Zorro <izorkoltsev@bk.ru>
2026-04-22 00:28:59 +0300

    Удалён дублирующийся CommentViewSet

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
-from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import permissions, viewsets
+from rest_framework import filters, permissions, viewsets
 from posts.models import Follow, Group, Post, Comment
 from .permissions import IsAuthorOrReadOnlyPermission
 from .serializers import (
@@ -25,7 +24,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class FollowViewSet(viewsets.ModelViewSet):
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [filters.SearchFilter]  # ← добавьте эту строку
+    filter_backends = [filters.SearchFilter]
     search_fields = ['following__username', 'user__username']
 
     def get_queryset(self):
@@ -34,19 +33,6 @@ class FollowViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
-class CommentViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthorOrReadOnlyPermission]
-
-    def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        return Comment.objects.filter(post_id=post_id)
-
-    def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(id=post_id)
-        serializer.save(author=self.request.user, post=post)
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
