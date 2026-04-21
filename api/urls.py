422e5ce3051c2d89e1d8cb233f98edeced486f72
Ilja Zorro <izorkoltsev@bk.ru>
2026-04-22 00:16:40 +0300

    Исправлен дублирующий импорт в urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, GroupViewSet, FollowViewSet
-
-from .views import FollowViewSet, GroupViewSet, PostViewSet
+from .views import PostViewSet, GroupViewSet, FollowViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register("posts", PostViewSet)
-router.register("groups", GroupViewSet)
-router.register("follow", FollowViewSet, basename="follow")
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path('', include(router.urls)),
+    path('posts/<int:post_id>/comments/',
+         CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
+         name='comment-list'),
+    path('posts/<int:post_id>/comments/<int:id>/',
+         CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}),
+         name='comment-detail'),
 ]
